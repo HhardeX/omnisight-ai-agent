dceb45816b13ec057d18be48c8f7d27c1b79074d
abhijeetbiswas119-ship-it <abhijeetbiswas119@gmail.com>
2026-09-18 15:24:42 +0530

    Allow Netlify frontend origin

--- app/main.py
+++ app/main.py
@@ -42,8 +42,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://jolly-stroopwafel-6c9e62.netlify.app",
     ],
     allow_credentials=True,
+    
     allow_methods=["*"],
     allow_headers=["*"],
 )
